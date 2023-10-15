09ead82ab7b5afbcfde4686b4f483e4087036871
Alexey Sharov <psypolice88@gmail.com>
2023-10-15 15:45:37 +0300

    edit/fix deprecated class

--- src/app/helper/auth-guard.service.ts
+++ src/app/helper/auth-guard.service.ts
@@ -1,22 +1,29 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
 import {Observable} from "rxjs";
 import {TokenStorageService} from "../service/token-storage.service";
+import {NotificationService} from "../service/notification.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService {
 
   constructor(private router: Router,
-              private tokenService: TokenStorageService) {
+              private tokenService: TokenStorageService,
+              private notification: NotificationService) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const currentUser = this.tokenService.getUser()
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const currentUser = this.tokenService.getUser();
     if (currentUser) {
       return true;
     }
+    this.notification.showSnackBar('Please Log in!');
     this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
     return false;
   }
